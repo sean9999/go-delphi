4db3205c9c51c458719552363f2bd5d4907931fa
Sean M <Sean.macdonald@hardrockdigital.com>
2024-10-21 10:44:54 -0400

    wip

--- message.go
+++ message.go
@@ -18,14 +18,17 @@ func (m Message) Valid() bool {
 	return (m.Encypted() && !m.Plain()) || (m.Plain() && !m.Encypted())
 }
 
+// returns true of CipherText is not null
 func (m Message) Encypted() bool {
 	return m.CipherText != nil && m.PlainText == nil
 }
 
+// returns true of PlainText is not nil
 func (m Message) Plain() bool {
 	return m.CipherText == nil && m.PlainText != nil
 }
 
+// sign a message
 func (m Message) Sign(randomness io.Reader, me *ecdh.PrivateKey) (sig []byte, nonce []byte, err error) {
 	return nil, nil, ErrNotImplemented
 }
