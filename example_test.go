626345f4e6a93082d8cf02d15499be6d79a7bd17
Sean Macdonald <sean.r.macdonald@gmail.com>
2025-06-10 22:55:28 -0400

    add runnable example

--- example_test.go
+++ example_test.go
@@ -2,6 +2,8 @@ package delphi_test
 
 import (
 	"crypto/rand"
+	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/sean9999/go-delphi"
@@ -40,5 +42,42 @@ func TestExample(t *testing.T) {
 	foo, ok := msg.Headers["foo"]
 	assert.True(t, ok)
 	assert.Equal(t, "bar", foo)
+
+}
+
+func Example() {
+
+	//	some plain text
+	sentence := []byte("hello world")
+
+	//	create two principals
+	alice := delphi.NewPrincipal(rand.Reader)
+	bob := delphi.NewPrincipal(rand.Reader)
+
+	//	create a message for bob, from alice
+	msg := delphi.NewMessage(rand.Reader, delphi.PlainMessage, sentence)
+	msg.SenderKey = alice.PublicKey()
+
+	//	add some metadata (this becomes AAD)
+	msg.Headers["foo"] = "bar"
+	msg.Headers["bing"] = "bat"
+
+	//	encrypt message
+	err := msg.Encrypt(rand.Reader, alice, bob.PublicKey(), nil)
+	fmt.Println("should be nil", err)
+
+	//	decrpyt message
+	err = bob.Decrypt(msg, nil)
+	fmt.Println("should be nil", err)
+
+	//	is decrypted text same as plain text?
+	diff := slices.Compare(sentence, msg.PlainText)
+	fmt.Println("should be 0", diff)
+
+	//	has the metadata survived?
+	foo, ok := msg.Headers["foo"]
+
+	fmt.Println("should be true", ok)
+	fmt.Println("should be bar", foo)
 
 }
